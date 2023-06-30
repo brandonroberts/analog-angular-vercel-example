af81edf9f7cd3b4cd56ef9a035f6b61c06780f0e
Brandon Roberts <robertsbt@gmail.com>
2023-06-30 08:38:11 -0500

    feat: update to latest Analog, Angular dependencies

--- analog-angular-vercel-example/vite.config.ts
+++ analog-angular-vercel-example/vite.config.ts
@@ -1,15 +1,13 @@
 /// <reference types="vitest" />
 
 import analog from '@analogjs/platform';
-import { visualizer } from 'rollup-plugin-visualizer';
-import { defineConfig, Plugin, splitVendorChunkPlugin } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     publicDir: 'src/public',
-
     optimizeDeps: {
       include: ['@angular/common', '@angular/forms'],
     },
@@ -18,40 +16,16 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [
       analog({
-        ssrBuildDir: '../dist/./analog-angular-vercel-example/ssr',
-        entryServer: './analog-angular-vercel-example/src/main.server.ts',
-        vite: {
-          inlineStylesExtension: 'css',
-          tsconfig:
-            mode === 'test'
-              ? './analog-angular-vercel-example/tsconfig.spec.json'
-              : './analog-angular-vercel-example/tsconfig.app.json',
-        },
         nitro: {
-          rootDir: './analog-angular-vercel-example',
           output: {
-            dir: '../../.vercel/output',
-            publicDir: '../../.vercel/output/static',
+            dir: '../../../.vercel/output',
+            publicDir: '../../../.vercel/output/static',
           },
-          publicAssets: [
-            { dir: `../../dist/./analog-angular-vercel-example/client` },
-          ],
-          serverAssets: [
-            {
-              baseName: 'public',
-              dir: `./dist/./analog-angular-vercel-example/client`,
-            },
-          ],
-          buildDir: '../dist/./analog-angular-vercel-example/.nitro',
-        },
-        prerender: {
-          routes: ['/'],
         },
       }),
       tsConfigPaths({
         root: '../',
       }),
-      visualizer() as Plugin,
       splitVendorChunkPlugin(),
     ],
     test: {
